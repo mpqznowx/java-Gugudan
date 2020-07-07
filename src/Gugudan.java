568b5e9c554a42b1941a069799b26a5b5890621f
박재성 <parkjaesung@solutis.co.kr>
2020-07-07 11:40:26 +0900

    사용자에게 숫자를 입력받아 구구단을 완성하는 Scanner 클래스 사

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Gugudan {
 	//메인 메소드가 반드시 필요
@@ -31,28 +32,45 @@ public class Gugudan {
 		//4단
 		//변수에 값을 담아 사용하는 방법
 		//int는 숫자만 저장할 수 있다.
-		System.out.println("4단 입니다");
-		int result = 4 * 1;
-		System.out.println(result);
-		result = 4 * 2;
-		System.out.println(result);
-		result = 4 * 3;
-		System.out.println(result);
-		result = 4 * 4;
-		System.out.println(result);
-		result = 4 * 5;
-		System.out.println(result);
-		result = 4 * 6;
-		System.out.println(result);
-		result = 4 * 7;
-		System.out.println(result);
-		result = 4 * 8;
-		System.out.println(result);
-		result = 4 * 9;
-		System.out.println(result);
-		result = 4 * 10;
-		System.out.println(result);
+//		System.out.println("4단 입니다");
+//		int result = 4 * 1;
+//		System.out.println(result);
+//		result = 4 * 2;
+//		System.out.println(result);
+//		result = 4 * 3;
+//		System.out.println(result);
+//		result = 4 * 4;
+//		System.out.println(result);
+//		result = 4 * 5;
+//		System.out.println(result);
+//		result = 4 * 6;
+//		System.out.println(result);
+//		result = 4 * 7;
+//		System.out.println(result);
+//		result = 4 * 8;
+//		System.out.println(result);
+//		result = 4 * 9;
+//		System.out.println(result);
+//		result = 4 * 10;
+//		System.out.println(result);
 		
+		// 사용자에게 숫자값을 입력 받는 방법
+		// Scanner 라는 클래스를 import를 해와서 사용 (jdk에서 기본적으로 제공하는 클래스)
+		//Scanner scanner = new Scanner(System.in);
+		//int number = scanner.nextInt();
+		System.out.println("구구단 중 출력할 단은 ? :");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		System.out.println(number * 1);
+		System.out.println(number * 2);
+		System.out.println(number * 3);
+		System.out.println(number * 4);
+		System.out.println(number * 5);
+		System.out.println(number * 6);
+		System.out.println(number * 7);
+		System.out.println(number * 8);
+		System.out.println(number * 9);
+		System.out.println(number * 10);
 		
 	}
 }

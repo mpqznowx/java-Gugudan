a97f8e40463200c55fc08ac6579faf78c14a3fe1
박재성 <parkjaesung@solutis.co.kr>
2020-07-07 11:32:48 +0900

    4단 소스 추가 - 변수 활

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -3,30 +3,56 @@ public class Gugudan {
 	//메인 메소드가 반드시 필요
 	public static void main(String[] args) {
 		// 2단
-		System.out.println("2단");
-		System.out.println(2 * 1);
-		System.out.println(2 * 2);
-		System.out.println(2 * 3);
-		System.out.println(2 * 4);
-		System.out.println(2 * 5);
-		System.out.println(2 * 6);
-		System.out.println(2 * 7);
-		System.out.println(2 * 8);
-		System.out.println(2 * 9);
-		System.out.println(2 * 10);
+//		System.out.println("2단");
+//		System.out.println(2 * 1);
+//		System.out.println(2 * 2);
+//		System.out.println(2 * 3);
+//		System.out.println(2 * 4);
+//		System.out.println(2 * 5);
+//		System.out.println(2 * 6);
+//		System.out.println(2 * 7);
+//		System.out.println(2 * 8);
+//		System.out.println(2 * 9);
+//		System.out.println(2 * 10);
 		
 		// 3단
-		System.out.println("3단");
-		System.out.println(3 * 1);
-		System.out.println(3 * 2);
-		System.out.println(3 * 3);
-		System.out.println(3 * 4);
-		System.out.println(3 * 5);
-		System.out.println(3 * 6);
-		System.out.println(3 * 7);
-		System.out.println(3 * 8);
-		System.out.println(3 * 9);
-		System.out.println(3 * 10);
+//		System.out.println("3단");
+//		System.out.println(3 * 1);
+//		System.out.println(3 * 2);
+//		System.out.println(3 * 3);
+//		System.out.println(3 * 4);
+//		System.out.println(3 * 5);
+//		System.out.println(3 * 6);
+//		System.out.println(3 * 7);
+//		System.out.println(3 * 8);
+//		System.out.println(3 * 9);
+//		System.out.println(3 * 10);
+		
+		//4단
+		//변수에 값을 담아 사용하는 방법
+		//int는 숫자만 저장할 수 있다.
+		System.out.println("4단 입니다");
+		int result = 4 * 1;
+		System.out.println(result);
+		result = 4 * 2;
+		System.out.println(result);
+		result = 4 * 3;
+		System.out.println(result);
+		result = 4 * 4;
+		System.out.println(result);
+		result = 4 * 5;
+		System.out.println(result);
+		result = 4 * 6;
+		System.out.println(result);
+		result = 4 * 7;
+		System.out.println(result);
+		result = 4 * 8;
+		System.out.println(result);
+		result = 4 * 9;
+		System.out.println(result);
+		result = 4 * 10;
+		System.out.println(result);
+		
 		
 	}
 }
